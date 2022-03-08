33ecf5aae5d3cc7b815a672d072462a06257e228
GeekTR <tr@geektang.cn>
2022-03-08 12:36:08 +0800

    Fix error tips.

--- app/src/main/java/cn/geektang/privacyspace/util/AppHelper.kt
+++ app/src/main/java/cn/geektang/privacyspace/util/AppHelper.kt
@@ -8,27 +8,37 @@ import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.collection.ArrayMap
 import cn.geektang.privacyspace.BuildConfig
 import cn.geektang.privacyspace.R
 import cn.geektang.privacyspace.bean.AppInfo
-import cn.geektang.privacyspace.util.AppHelper.loadAllAppList
+import com.microsoft.appcenter.analytics.Analytics
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlin.jvm.Throws
 
 object AppHelper {
     private val _allApps = MutableStateFlow<List<AppInfo>>(emptyList())
     val allApps: Flow<List<AppInfo>> = _allApps
+    private var getAppsRetryTimes = 0
 
     suspend fun initialize(context: Context) {
         val apps = try {
-            context.loadAllAppList()
+            val result = context.loadAllAppList()
+            getAppsRetryTimes = 0
+            result
         } catch (e: Exception) {
+            val properties = ArrayMap<String, String>()
+            properties["exception"] = e.javaClass.name
+            Analytics.trackEvent("ReadAppsFailed", properties)
+            getAppsRetryTimes++
+
             e.printStackTrace()
-            context.showToast(R.string.tips_get_apps_failed)
-            delay(3000)
-            // retry after 3 seconds
+            if (getAppsRetryTimes == 3) {
+                context.showToast(R.string.tips_get_apps_failed)
+            }
+            delay(1000)
+            // retry after 1 seconds
             initialize(context)
             return
         }
